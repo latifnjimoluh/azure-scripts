b61016a85f74607f93e2ca1a3491dd559b9ab93c
latifnjimoluh <159473076+latifnjimoluh@users.noreply.github.com>
2025-05-30 09:57:55 +0100

    first commit

---
+++ install_nginx.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+apt-get update && apt-get install -y nginx
+cat <<"EOF" > /etc/nginx/sites-available/default
+server {
+    listen 80;
+    location / {
+        proxy_pass http://10.0.2.4:5000;
+        proxy_set_header Host \$host;
+        proxy_set_header X-Real-IP \$remote_addr;
+    }
+}
+EOF
+systemctl restart nginx
